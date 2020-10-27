cb474599293893ddccb9f13285ddb3ad5e49a06b
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-10-27 11:37:41 +0000

    Add dev dependencies to build the documentation

--- istacpy/docs/source/conf.py
+++ istacpy/docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('../../../'))
 
 
@@ -30,10 +31,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-	'sphinx.ext.napoleon',
-	'sphinx.ext.autodoc'
-]
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -61,4 +59,4 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
