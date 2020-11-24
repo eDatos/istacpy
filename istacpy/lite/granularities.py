19a006b4d4472ecf858f8c8a41eddf7732c3a571
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-11-24 10:49:37 +0000

    Add granularities for geographic, time and measure dimensions

---
+++ istacpy/lite/granularities.py
@@ -0,0 +1,27 @@
+GEOGRAPHICAL = {
+    'T': 'COUNTRIES',
+    'R': 'REGIONS',
+    'I': 'ISLANDS',
+    'P': 'PROVINCES',
+    'C': 'COUNTIES',
+    'M': 'MUNICIPALITIES',
+}
+
+TIME = {
+    'Y': 'YEARLY',
+    'B': 'BIYEARLY',
+    'Q': 'QUARTERLY',
+    'F': 'FOUR_MONTHLY',
+    'M': 'MONTHLY',
+    'W': 'WEEKLY',
+    'D': 'DAILY',
+    'H': 'HOURLY',
+}
+
+MEASURE = {
+    'A': 'ABSOLUTE',
+    'N': 'ANNUAL_PERCENTAGE_RATE',
+    'I': 'INTERPERIOD_PERCENTAGE_RATE',
+    'M': 'ANNUAL_PUNTUAL_RATE',
+    'J': 'INTERPERIOD_PUNTUAL_RATE',
+}
