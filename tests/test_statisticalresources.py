e48f00d81ac2ac2948aadc40d7680076a8d2327e
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-04-29 11:24:23 +0100

    Add tests for statistical queries

--- tests/test_statisticalresources.py
+++ tests/test_statisticalresources.py
@@ -1,4 +1,4 @@
-from istacpy.statisticalresources import cubes
+from istacpy.statisticalresources import cubes, queries
 
 from .services import assert_valid_response
 
@@ -34,5 +34,29 @@ def test_get_statisticalresources_datasets_agency_resource_version():
         version='2.2',
         dim='TIME_PERIOD:2010',
         fields='-metadata',
+    )
+    assert_valid_response(response)
+
+
+def test_get_statisticalresources_queries():
+    response = queries.get_statisticalresources_queries(
+        orderby='id ASC', query='name ILIKE "PASAJEROS"'
+    )
+    assert_valid_response(response)
+
+
+def test_get_statisticalresources_queries_agency():
+    response = queries.get_statisticalresources_queries_agency(
+        agencyid='ISTAC', orderby='id ASC', query='name ILIKE "PASAJEROS"'
+    )
+    assert_valid_response(response)
+
+
+def test_get_statisticalresources_queries_agency_resource():
+    response = queries.get_statisticalresources_queries_agency_resource(
+        agencyid='ISTAC',
+        resourceid='C00017A_000001',
+        orderby='id ASC',
+        query='name ILIKE "PASAJEROS"',
     )
     assert_valid_response(response)
