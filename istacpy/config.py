82383eabf123e2d216f6e5a8c2810125486b8606
DAVID VARGAS RUIZ <dvarrui@gobiernodecanarias.org>
2025-09-05 12:57:44 +0100

    [U] Incluir comentarios sobre API_KEY

--- istacpy/config.py
+++ istacpy/config.py
@@ -2,5 +2,8 @@ API_ROOT_URL = 'https://datos.canarias.es/api/estadisticas/'
 API_VERSION = 1.0
 VALUE_ERROR = 'NaN'
 DEBUG = False
+# TODO: Cambiar el nombre API_KEY para evitar confusiones.
+# - El contenido API_KEY no hace referencia a un token de acceso.
+# - Realmente se usa para redireccionar las consultas.
 ISTAC_API_KEY = 'wsPhbRSy6feNO9ZThnJon83UqpIJuN6JIIyRZ7jXmDKH9ExqS40e5nJ9Dr8xoqKM'
 IBESTAT_API_KEY = 'C977V5bSkaJ3kSszESV2cVLoC77tviDrjiLjjsjNUMSHptxR18Zr66kAyJbPZEu4'
